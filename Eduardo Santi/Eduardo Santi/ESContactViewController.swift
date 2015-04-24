efa7975633506d1ee597ee921038e70fa4f6a4b0
Eduardo Santi <eduardo.senacrs@gmail.com>
2015-04-24 16:46:44 -0300

    add view contact and icons in tab bar

--- Eduardo Santi/Eduardo Santi/ESContactViewController.swift
+++ Eduardo Santi/Eduardo Santi/ESContactViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class ESContactViewController: UIViewController {
+class ESContactViewController: UIViewController, MFMailComposeViewControllerDelegate {
     
     @IBOutlet weak var labelMyName: UILabel!
     @IBOutlet weak var labelMyInformation: UILabel!
@@ -19,6 +20,35 @@ class ESContactViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sendEmail(sender: AnyObject) {
+        var emailTitle = ""
+        var messageBody = "\n\n\n\nE-mail send from the Eduardo Santi app"
+        var toRecipents = ["eduardo.bepid@gmail.com"]
+        var mc: MFMailComposeViewController = MFMailComposeViewController()
+        mc.mailComposeDelegate = self
+        mc.setSubject(emailTitle)
+        mc.setMessageBody(messageBody, isHTML: false)
+        mc.setToRecipients(toRecipents)
+        
+        self.presentViewController(mc, animated: true, completion: nil)
+    }
+    
+    func mailComposeController(controller:MFMailComposeViewController, didFinishWithResult result:MFMailComposeResult, error:NSError) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func call(sender: AnyObject) {
+        let phone = "tel://+555185319677";
+        let url: NSURL = NSURL(string: phone)!;
+        UIApplication.sharedApplication().openURL(url);
+    }
+    
+    @IBAction func goGithub(sender: AnyObject) {
+        let github = "http://github.com/santieduardo";
+        let url: NSURL = NSURL(string: github)!;
+        UIApplication.sharedApplication().openURL(url);
     }
     
 }
